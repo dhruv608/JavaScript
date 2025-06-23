b60483a05926f17c5899e0a61184893bf2eb5be2
Dhruv narang <dhruvnarang608@gmail.com>
2025-06-23 21:37:46 +0000

    summaryOfdataypes

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,23 @@
+// primitive
+// 7 types : String , Number , Boolean , null  , undefined, Symbol 
+// , BigInt
+
+// dynamic type language
+
+// bigint 
+ const BigNumber = 929849893289428942894289n
+// last mai n lga do bignit krne ke ley
+
+// reference type or non primitive
+//arays and objects , function
+
+const heros = ["shaktiman","naagraj","doga"]
+let myobj ={
+    name : "dhruv",
+    age : 19,
+}
+const myFunction = function(){
+    console.log("Hello world")
+}
+
+console.log(typeof BigNumber)
