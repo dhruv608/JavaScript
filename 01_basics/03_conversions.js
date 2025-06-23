f8222eabb8299a88c69dba2fae76e8d354c40022
Dhruv narang <dhruvnarang608@gmail.com>
2025-06-23 21:01:58 +0000

    conversionOperations

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,22 @@
+let score = "53a"
+
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber) // if score is not in number like 54r so it converts but give NaN
+
+// "33" => 33
+// "33a" => NaN
+// true =>1 , false => 0;
+ 
+let isLoggedIn =9;
+let booleanIsLoggedIn = Boolean(isLoggedIn)  
+// 1 => true/ 0 => false
+// "xyz" => true / "" => false
+
+// console.log(typeof(booleanIsLoggedIn));
+// console.log(booleanIsLoggedIn);
+let someNumber =99
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber) // > string 
