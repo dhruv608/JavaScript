69a12a856858a9437de736c51da5da7c684db6fa
Dhruv narang <dhruvnarang608@gmail.com>
2025-06-23 21:56:30 +0000

    commit again

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountMail = "dhruvnarang608@gmail.com"
+var accountPassoword = "12345"
+accountCity ="Punjab" //
+let accountState;  // undefined means not yet defined
+
+// accountId =2 not allowed to change const
+/*
+prefer not to use beacuase of issue of function block
+*/
+accountMail = "hitish@gmail.com"
+accountCity = "Bangalore"
+accountPassoword = "2005"
+console.table([accountMail ,accountId,accountPassoword,accountCity ,accountState])
+
