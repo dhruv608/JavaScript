3950a2f96aeb636deaa34c6685c94769a66db0bf
Dhruv narang <dhruvnarang608@gmail.com>
2025-06-23 21:56:50 +0000

    again

---
+++ 01_basics/04_comparsions.js
@@ -0,0 +1,16 @@
+// console.log(2>1)
+// console.log(1==1)
+// console.log(2!=2)
+
+console.log("2">1)
+
+console.log(null >0)
+console.log(null ==0)
+console.log(null >=0) // < >= <= converts null to 0 thats why here  true
+
+console.log(undefined>0)
+console.log(undefined ==0)
+console.log(undefined <0)
+
+// === strict check 
+console.log("2"==2) // checks datatype also
